280654f150c798212df9fdcc17b54fb30534e8a4
Salvador Villalon <salvav1@uci.edu>
2020-05-28 12:58:25 -0700

    remove firebase config from gitignore

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyDt_kG6N3wB-k5FmeHIuOUDCXa8WGUK-Y4",
+    authDomain: "theodinproject-library.firebaseapp.com",
+    databaseURL: "https://theodinproject-library.firebaseio.com",
+    projectId: "theodinproject-library",
+    storageBucket: "theodinproject-library.appspot.com",
+    messagingSenderId: "647767416859",
+    appId: "1:647767416859:web:3e4ecd92e8bbb33c1918be",
+    measurementId: "G-7V9WKTLST8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
